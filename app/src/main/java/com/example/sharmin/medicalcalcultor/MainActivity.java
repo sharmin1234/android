596aa245fc0a2342a0ee9d63ab020d45b7f7853c
sharmin1234 <munasharmin7140@gmailcom>
2018-09-08 01:11:53 +0600

    first commit

---
+++ app/src/main/java/com/example/sharmin/medicalcalcultor/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.sharmin.medicalcalcultor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onKgToPoundClick(View view) {
+    }
+
+    public void onPoundToKgClick(View view) {
+    }
+}
